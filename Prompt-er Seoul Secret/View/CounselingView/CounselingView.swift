bec50cfb9ba375f60b18537daba82f5853d517d5
seunggyun-jeong <jskyun98@gmail.com>
2023-09-02 01:40:02 +0900

    결과 뷰 레이아웃 정리

--- Prompt-er Seoul Secret/View/CounselingView/CounselingView.swift
+++ Prompt-er Seoul Secret/View/CounselingView/CounselingView.swift
@@ -19,33 +19,41 @@ struct CounselingView: View {
                     .shadow(color: .black.opacity(0.14), radius: 14, x: 0, y: 0)
                     .padding(.bottom, 70)
                 
-                Text("사용된 색상")
-                    .font(.title)
-                    .bold()
-                    .padding(.bottom, 27)
-                
-                HStack(spacing: 28) {
-                    ForEach(report.colors, id: \.id) { color in
-                        Circle()
-                            .frame(width: 65, height: 65)
-                            .foregroundColor(Color(uiColor: color.uiColor))
+                ZStack {
+                    Color.white
+                    VStack {
+                        Text("사용된 색상")
+                            .font(.title)
+                            .bold()
+                            .padding(.bottom, 27)
+                        
+                        HStack(spacing: 28) {
+                            ForEach(report.colors, id: \.id) { color in
+                                Circle()
+                                    .frame(width: 65, height: 65)
+                                    .foregroundColor(Color(uiColor: color.uiColor))
+                            }
+                        }
+                        .padding(.bottom, 72)
+                        
+                        Divider()
+                            .padding(.bottom, 72)
+                        
+                        Text("대화 내용 요약")
+                            .font(.title)
+                            .bold()
+                            .padding(.bottom, 64)
+                        
+                        Text(report.recordSummary)
+                            .font(.title2)
                     }
+                    .padding(.horizontal, 88)
+                    .padding(.vertical, 71)
                 }
-                .padding(.bottom, 63)
-                
-                Divider()
-                    .padding(.bottom, 81)
-                    .padding(.horizontal, 104)
-                
-                Text("대화 내용 요약")
-                    .font(.title)
-                    .bold()
-                    .padding(.bottom, 56)
-                
-                Text(report.recordSummary)
-                    .padding(.horizontal, 104)
-                    .font(.title2)
+                .padding(.horizontal, 57)
+                .shadow(color: .black.opacity(0.1), radius: 14, x: 0, y: 10)
             }
+            .background(Color.init(red: 248, green: 248, blue: 248))
             .navigationTitle("\(report.name)님의 작품")
             .navigationBarTitleDisplayMode(.inline)
             .toolbar {
@@ -58,11 +66,11 @@ struct CounselingView: View {
         }
     }
 }
-//
-//struct CounselingView_Previews: PreviewProvider {
-//    static var previews: some View {
-//        NavigationStack {
-//            CounselingView(report: ReportModel(name: "", date: "", recordSummary: "", colors: [], imageUrl: URL(fileURLWithPath: "")), image: UIImage(named: "ColoringBookEx")!, painterName: "을룡태", counselingContent: "어쩌구 저쩌구 어쩌구 저쩌구어쩌구 저쩌구어쩌구 저쩌구어쩌구 저쩌구어쩌구 저쩌구어쩌구 저쩌구어쩌구 저쩌구어쩌구 저쩌구어쩌구 저쩌구어쩌구 저쩌구어쩌구 저쩌구어쩌구 저쩌구어쩌구 저쩌구어쩌구 저쩌구어쩌구 저쩌구어쩌구 저쩌구어쩌구 저쩌구어쩌구 저쩌구어쩌구 저쩌구어쩌구 저쩌구어쩌구 저쩌구어쩌구 저쩌구어쩌구 저쩌구어쩌구 저쩌구어쩌구 저쩌구어쩌구 저쩌구어쩌구 저쩌구어쩌구 저쩌구어쩌구 저쩌구어쩌구 저쩌구어쩌구 저쩌구어쩌구 저쩌구어쩌구 저쩌구어쩌구 저쩌구어쩌구 저쩌구어쩌구 저쩌구어쩌구 저쩌구어쩌구 저쩌구어쩌구 저쩌구어쩌구 저쩌구어쩌구 저쩌구어쩌구 저쩌구어쩌구 저쩌구어쩌구 저쩌구어쩌구 저쩌구어쩌구 저쩌구어쩌구 저쩌구어쩌구 저쩌구어쩌구 저쩌구어쩌구 저쩌구어쩌구 저쩌구어쩌구 저쩌구어쩌구 저쩌구어쩌구 저쩌구어쩌구 저쩌구어쩌구 저쩌구어쩌구 저쩌구어쩌구 저쩌구어쩌구 저쩌구어쩌구 저쩌구어쩌구 저쩌구")
-//        }
-//    }
-//}
+
+struct CounselingView_Previews: PreviewProvider {
+    static var previews: some View {
+        NavigationStack {
+            CounselingView(report: .init(name: "", date: "", recordSummary: "어쩌구 저쩌구 어쩌구 저쩌구어쩌구 저쩌구어쩌구 저쩌구어쩌구 저어쩌구 저쩌구 어쩌구 저쩌구어쩌구 저쩌구어쩌구 저쩌구어쩌구 저어쩌구 저쩌구 어쩌구 저쩌구어쩌구 저쩌구어쩌구 저쩌구어쩌구 저어쩌구 저쩌구 어쩌구 저쩌구어쩌구 저쩌구어쩌구 저쩌구어쩌구 저", colors: [], imageUrl: URL(filePath: "")))
+        }
+    }
+}
